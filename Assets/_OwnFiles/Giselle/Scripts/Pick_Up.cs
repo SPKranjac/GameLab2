1c71805e28191abda222928d9e33a6d732c44295
Steven <spkranjac@gmail.com>
2020-07-10 11:55:17 +1000

    PickUp changes
    - WIP for fixing pickups

--- Assets/_OwnFiles/Giselle/Scripts/Pick_Up.cs
+++ Assets/_OwnFiles/Giselle/Scripts/Pick_Up.cs
@@ -3,9 +3,15 @@
 public class Pick_Up : MonoBehaviour
 {
     public Transform onHand;
+    Rigidbody Cube;
 
+    public void Start()
+    {
+        Cube = GetComponent<Rigidbody>(); 
+    }
     private void OnMouseDown()
     {
+        Cube.constraints = RigidbodyConstraints.FreezePosition;
         GetComponent<Rigidbody>().useGravity = false;
         this.transform.position = onHand.transform.position;
         transform.parent = GameObject.Find("Player").transform;
@@ -14,8 +20,10 @@ public class Pick_Up : MonoBehaviour
 
     private void OnMouseUp()
     {
+        Cube.constraints = RigidbodyConstraints.None;
         this.transform.parent = null;
         GetComponent<Rigidbody>().useGravity = true;
+        Cube.constraints = RigidbodyConstraints.None;
     }
 
 }
